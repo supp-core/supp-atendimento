3f5800bae1da91c18cf471a3b62c2970ec2c989d
Danilo de Souza Lima <dannilocomp@gmail.com>
2025-01-18 13:29:00 -0300

    atualizacao menu

--- supp-atendimento-front-end/src/components/common/AppHeader.vue
+++ supp-atendimento-front-end/src/components/common/AppHeader.vue
@@ -17,16 +17,31 @@
   
   <style scoped>
   .header {
-    background: white;
-    padding: 1rem;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    box-shadow: 0 1px 3px rgba(0,0,0,0.1);
+    position: fixed; /* Fixa o cabeçalho no topo */
+  top: 0;
+  left: 0;
+  right: 0; /* Garante que ocupe toda a largura */
+  background: white;
+  padding: 1rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  box-shadow: 0 1px 3px rgba(0,0,0,0.1);
+  z-index: 1000; /* Garante que fique acima dos outros elementos */
+  height: 60px; /* Altura fixa para alinhar com a sidebar */
   }
   
   .logo-img {
     height: 40px;
     width: auto;
   }
+
+
+  .dashboard {
+  padding-top: 60px; /* Mesma altura do header */
+  min-height: 100vh;
+  background-color: #f3f4f6;
+}
+
   </style>
+  
